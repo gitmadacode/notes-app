7f6f07c06fd3510be3e9cdb3f26a67eaf1fef9e4
madafakaoficial <sebastiangodoy45@hotmail.com>
2019-10-07 16:21:21 -0300

    Version 3.0

--- .history/src/database_20190818202556.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost/node-notes-db', {
-  useCreateIndex: true,
-  useNewUrlParser: true                                //esto es para el funcionamiento de la biblioteca
-})
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.error(err));
